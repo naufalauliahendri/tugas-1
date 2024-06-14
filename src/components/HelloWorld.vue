c5fa5dc44ee8b5441d95204eba48445e34296cf9
naufalauliahendri <naufalauliahendri@student.uir.ac.id>
2024-06-14 16:19:24 +0700

    tugas 1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      <a href="" target="" rel="noopener">Contact: <br> +6281364103405</a><br>
+      <a href="https://api.whatsapp.com/send/?phone=%2B6281364103405&text&type=phone_number&app_absent=0" target="_blank" rel="noopener">081364103405</a> <br>
+      <a href="https://www.instagram.com/naufalaulia_?igsh=NDF5cXltbzc2ZHVz" target="_blank" rel="noopener">naufalaulia_</a><br>
+      <a href="https://mail.google.com/mail/u/0/#inbox" target="_blank" rel="noopener">naufalaulia140704@gmail.com</a><br>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Pendidikan</template>
+MI DARUL IHSAN<br>
+SMP NEGERI 9 MANDAU<br>
+SMA NEGERI 3 MANDAU<br>
+TEKNIK INFORMATIKA UNIVERSITAS ISLAM RIAU
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Specialist</template>
+    Software Engineer
+    memahami cara-cara pengembangan peranti lunak termasuk pembuatan, pemeliharaan, manajemen organisasi pengembangan peranti lunak dan manajemen mutu.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Hobi</template>
+Olahraga(Volly, badminton, jogging)<br>
+Bermain Game<br>
+Nonton<br>
+Mendengar Musik
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Community</template>
+Tergabung dalam komunitas:
+<ul>
+  <li>Teknik Informatika</li>
+  <li>Data Analyst Indonesia</li>
+  <li>UI/UX Designer Indonesia</li>
+</ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Status</template>
+
+    Single (jomblo)
+  </WelcomeItem>
+</template>
